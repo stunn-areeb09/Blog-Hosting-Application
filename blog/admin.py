0ae137fd5e043675ae8b542d8228cea994095a8d
Syed Areeb Wadood <areeb.wadood9@gmail.com>
2021-01-26 16:13:15 +0530

    Added S_post and E_post model for sports and Entertainment category

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, S_Post  , E_Post
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(S_Post)
+admin.site.register(E_Post)
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, S_Post, E_Post
 
 def index(request):
 	return render( request , 'blog/index.html' )
 	
 def business(request):
-	context  =
-	{
+	context  ={
 		'posts' : Post.objects.all()
 	}
 	return render( request , 'blog/business.html' , context)
 
 def entertainment(request):
-	context = 
-	{
+	context = {
 		'posts' : E_Post.objects.all()
 	}
 	return render( request , 'blog/entertainment.html' , context )
 
 def sports(request):
-	context 
-	{
+	context =  {
 		'posts' : S_Post.objects.all()
 	}
 	return render( request  , 'blog/sports.html' , context)
