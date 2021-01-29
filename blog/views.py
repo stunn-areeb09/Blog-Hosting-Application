287d4f881f39fe7205c96982c0f590cd5ab6a756
Syed Areeb Wadood <areeb.wadood9@gmail.com>
2021-01-29 21:19:01 +0530

    Changed color of Submit button

--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,7 @@ def business_post_add(request):
 		business_post_user = request.user
 	business = Post( title = business_post_title , content = business_post_content  , author = business_post_user )
 	business.save()
-	messages.success( request , 'The blog with the specified title is successfully created !! ' )
+	messages.success( request , 'The blog with the  title %s is created successfully !! ' %business_post_title)
 	return render( request , 'blog/index.html' )
 
 def sports_post_add(request):
@@ -41,7 +41,7 @@ def sports_post_add(request):
 		sports_post_user = request.user
 	sports = S_Post( title = sports_post_title , content = sports_post_content , author = sports_post_user )
 	sports.save()
-	messages.success( request , 'The blog with the specified title is successfully created !! ')
+	messages.success( request , 'The blog with the  title %s is created successfully !! ' %sports_post_title)
 	return render ( request , 'blog/index.html' )
 
 def entertainment_post_add(request):
@@ -51,5 +51,5 @@ def entertainment_post_add(request):
 		entertainment_post_user = request.user
 	entertianment = E_Post( title = entertainment_post_title, content = entertainment_post_content , author =  entertainment_post_user )
 	entertianment.save()
-	messages.success( request , 'The blog with the specified title is created successfully !! ')
+	messages.success( request , 'The blog with the  title %s is created successfully !! ' %entertainment_post_title)
 	return render ( request ,  'blog/index.html' )
