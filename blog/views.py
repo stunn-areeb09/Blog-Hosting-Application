232d9f9a06d0d08e55c3f63507fcf5218822e17f
Syed Areeb Wadood <areeb.wadood9@gmail.com>
2021-04-19 13:04:41 +0530

    Changed messages.success() message being shown with proper appropriate category [earlier missing]

--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,7 @@ def business_post_add(request):
 		business_post_user = request.user
 	business = Post( title = business_post_title , content = business_post_content  , author = business_post_user )
 	business.save()
-	messages.success( request , 'The blog with the  title %s is created successfully !! ' %business_post_title)
+	messages.success( request , 'The blog with the  title %s is created successfully in the Bussiness Category !! ' %business_post_title)
 	return render( request , 'blog/index.html' )
 
 def sports_post_add(request):
@@ -44,7 +44,7 @@ def sports_post_add(request):
 		sports_post_user = request.user
 	sports = S_Post( title = sports_post_title , content = sports_post_content , author = sports_post_user )
 	sports.save()
-	messages.success( request , 'The blog with the  title %s is created successfully !! ' %sports_post_title)
+	messages.success( request , 'The blog with the  title %s is created successfully in the Sports Category !! ' %sports_post_title)
 	return render ( request , 'blog/index.html' )
 
 def entertainment_post_add(request):
@@ -54,7 +54,7 @@ def entertainment_post_add(request):
 		entertainment_post_user = request.user
 	entertianment = E_Post( title = entertainment_post_title, content = entertainment_post_content , author =  entertainment_post_user )
 	entertianment.save()
-	messages.success( request , 'The blog with the  title %s is created successfully !! ' %entertainment_post_title)
+	messages.success( request , 'The blog with the  title %s is created successfully in the Entertainment Category !! ' %entertainment_post_title)
 	return render ( request ,  'blog/index.html' )
 
 def updatebussiness( request , pk ):
@@ -132,7 +132,7 @@ def deleteentertainment( request , pk ):
 	if request.method == 'POST':
 		deleted_title = current_post.title
 		current_post.delete()
-		messages.success( request , 'The blog with the title {} is deleted successfully in Sports Category by user {} !!'.format(deleted_title , request.user))
+		messages.success( request , 'The blog with the title {} is deleted successfully in Entertianment Category by user {} !!'.format(deleted_title , request.user))
 		return redirect('/')
 	context = {
 		'posts' : current_post
